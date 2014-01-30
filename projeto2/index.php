610b8ee5509ef9cfd7f40479c38ce5d977cfee19
crysthiano <crysthianophp@gmail.com>
2014-01-30 13:48:57 -0200

    estudos css

---
+++ projeto2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>2 projeto curso bootstrap</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen" >
+        
+        <!-- HTML5 compativel com IE8 -->
+        <!--[if 1t IE 9]>
+            <script src="/js/html5shiv.js"></script>
+            <script src="/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <div class="row" id="topo">
+                <div class="col-md-5">
+                    <a href="index.php">
+                        <img src="img/logo-bootstrap.gif" class="img-responsive">
+                    </a>
+                </div>
+                <div class="col-md-7">
+                    <a href="#" class="btn btn-danger">Popouver</a>
+                    <a href="#myModal">Minha Modal</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
